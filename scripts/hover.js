c7b68737257d01cff59c3e3a4c54bb14da9840a7
ilsilentii <travis.vdm@outlook.com>
2017-11-09 13:09:42 -0700

    added up and down arrows registration

--- scripts/hover.js
+++ scripts/hover.js
@@ -16,4 +16,32 @@ $(document).ready(function(){
     $("#onHover")[0].play();
   }
   });
+
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 38:
+        if (!$( ".select2" ).hasClass( "pulse" )) {
+        $(".select2").addClass("pulse");
+        $(".select1").removeClass("pulse");
+        $("#onHover")[0].play();
+      } else {
+        $(".select1").addClass("pulse");
+        $(".select2").removeClass("pulse");
+        $("#onHover")[0].play();
+      }
+            break;
+        case 40:
+        if (!$( ".select2" ).hasClass( "pulse" )) {
+        $(".select2").addClass("pulse");
+        $(".select1").removeClass("pulse");
+        $("#onHover")[0].play();
+      } else {
+        $(".select1").addClass("pulse");
+        $(".select2").removeClass("pulse");
+        $("#onHover")[0].play();
+      }
+            break;
+    }
+};
+
 });
