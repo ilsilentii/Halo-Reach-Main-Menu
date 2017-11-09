c690bef5156df895eb050855d321c7c9b0ffacad
ilsilentii <travis.vdm@outlook.com>
2017-11-09 01:04:12 -0700

    added menu

--- scripts/sound.js
+++ scripts/sound.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
   setTimeout(function(){
       $("#rainSoundEffect").animate({volume: 0.03}, 7000);
   }, 1000);
+
+  $("#onHover")[0].volume = 0.1;
+  $(".select1").mouseenter(function() {
+  $("#onHover")[0].play();
+});
+$(".select2").mouseenter(function() {
+$("#onHover")[0].play();
+});
+
 });
