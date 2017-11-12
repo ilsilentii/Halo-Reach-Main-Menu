9ea900a4772147cc9e96a8260ffd18dc9aa6bef8
ilsilentii <travis.vdm@outlook.com>
2017-11-12 13:01:20 -0700

    moved campaign file

--- scripts/startmenu.js
+++ scripts/startmenu.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("#campaign").click(function(){
     $("body").removeClass("back-row-toggle start");
     $( "body" ).addClass( "campaign" );
+    $( "#startMenu" ).remove();
     $("#container").load("https://ilsilentii.github.io/Halo-Reach-Main-Menu/campaign.html");
       $("#rainSoundEffect").animate({volume: 0}, 5000);
     setTimeout(function(){
