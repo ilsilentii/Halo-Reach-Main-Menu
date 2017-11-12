81bcfe3c92fa6d1acf6a7c86b452191595249075
ilsilentii <travis.vdm@outlook.com>
2017-11-12 13:11:43 -0700

    changed url

--- scripts/startmenu.js
+++ scripts/startmenu.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("body").removeClass("back-row-toggle start");
     $( "body" ).addClass( "campaign" );
     $( "#startMenu" ).remove();
-    $("#container").load("https://ilsilentii.github.io/Halo-Reach-Main-Menu/campaign.html");
+    $("#container").load("https://ilsilentii.github.io/reach/html/campaign.html");
       $("#rainSoundEffect").animate({volume: 0}, 5000);
     setTimeout(function(){
         $( "#rainSoundEffect" ).remove();
